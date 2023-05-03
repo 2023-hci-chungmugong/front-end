20b5ab84d6ebf173fe024226b8fe089f9990ad9f
Jeong Jae-Seong <gik1819@gmail.com>
2023-05-03 22:35:17 +0900

    :recycle: 멤버의 자료형 정보 추가

--- lib/util/design_kit.dart
+++ lib/util/design_kit.dart
@@ -2,17 +2,17 @@ import 'package:flutter/material.dart';
 
 class DesignKit {
   // colors
-  static const mainColor = Color.fromARGB(255, 17, 22, 160);
-  static const green = Color.fromARGB(255, 162, 255, 171);
-  static const blue = Color.fromARGB(255, 147, 229, 255);
-  static const red = Color.fromARGB(255, 254, 161, 161);
-  static const yellow = Color.fromARGB(255, 255, 215, 0);
-  static const gray = Color.fromARGB(255, 188, 188, 188);
+  static const Color mainColor = Color.fromARGB(255, 17, 22, 160);
+  static const Color green = Color.fromARGB(255, 162, 255, 171);
+  static const Color blue = Color.fromARGB(255, 147, 229, 255);
+  static const Color red = Color.fromARGB(255, 254, 161, 161);
+  static const Color yellow = Color.fromARGB(255, 255, 215, 0);
+  static const Color gray = Color.fromARGB(255, 188, 188, 188);
 
   // proportions
   // figma에 있는 컴포넌트 사이즈를 넣으면 기기에 맞는 비율로 변환합니다.
-  static getWidth(context, width) =>
+  static double getWidth(context, width) =>
       MediaQuery.of(context).size.width * width / 360;
-  static getHeight(context, hegiht) =>
+  static double getHeight(context, hegiht) =>
       MediaQuery.of(context).size.height * hegiht / 800;
 }
