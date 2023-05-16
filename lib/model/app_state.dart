3d0ca0102cdd98d74baa3ad20ed63795d8652186
Jeong Jae-Seong <gik1819@gmail.com>
2023-05-17 00:32:41 +0900

    :sparkles: Model 설계 완성
    버그는 아직 안잡음

--- lib/model/app_state.dart
+++ lib/model/app_state.dart
@@ -1,10 +1,19 @@
+import 'package:chungmugong_front_end/model/abusing.dart';
+import 'package:chungmugong_front_end/model/reservation.dart';
 import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
+  // 5~9라인 써주면 인스턴스가 Sigletone으로 생성됨
   static final AppState _instance = AppState._internal();
 
   factory AppState() => _instance;
   AppState._internal();
 
-  // 여기에 모델(전역변수) 추가
+  // 아래에 모델(전역변수) 추가
+  DateTime appDate = DateTime.now();
+
+  // 아래 변수는 추후 Firebase와 연결하여 초기화 되어야함
+  ReservationForDate reservations = ReservationForDate(DateTime.now());
+  List<Abusing> abusingLog = [];
+  List<ReservationForUser> myReservations = [];
 }
