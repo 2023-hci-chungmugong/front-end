271f4e68e2a91ed4a8972652696b92be441575e6
csk6314 <37896060+csk6314@users.noreply.github.com>
2023-05-19 15:28:38 +0900

    [feature/drawer] drawer header

--- lib/view/lobby_view.dart
+++ lib/view/lobby_view.dart
@@ -19,15 +19,66 @@ class LobbyView extends StatelessWidget {
             '2023년 4월 29일 (토)',
             textColor: Colors.white,
           ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                'assets/icons/hamburger.svg',
-                width: DesignKit.getWidth(context, 24),
-                height: DesignKit.getHeight(context, 21),
-              ),
-            ),
+          actions: [],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          children: [
+            SizedBox(
+              height: DesignKit.getHeight(context, 260),
+              width: DesignKit.getWidth(context, 400),
+              child: DrawerHeader(
+                  child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset("assets/images/no_profile.png"),
+                  Text('학번',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                  Text('이름',
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w700)),
+                  SizedBox(
+                    height: DesignKit.getHeight(context, 30),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: DesignKit.getWidth(context, 20),
+                        height: DesignKit.getHeight(context, 25),
+                        margin: EdgeInsets.only(
+                            right: DesignKit.getWidth(context, 7)),
+                        decoration: BoxDecoration(
+                          color: DesignKit.yellow,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      Container(
+                        width: DesignKit.getWidth(context, 20),
+                        height: DesignKit.getHeight(context, 25),
+                        margin: EdgeInsets.only(
+                            right: DesignKit.getWidth(context, 7)),
+                        decoration: BoxDecoration(
+                          color: DesignKit.gray,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                      Container(
+                        width: DesignKit.getWidth(context, 20),
+                        height: DesignKit.getHeight(context, 25),
+                        decoration: BoxDecoration(
+                          color: DesignKit.gray,
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              )),
+            )
           ],
         ),
       ),
