25155f93e28f8a8d8e022c2dd7e27604fce464f5
Jae-Seong Jeong <44183028+Meronanana@users.noreply.github.com>
2023-06-11 21:26:48 +0900

    :sparkles: 예약기능 완성 (#12)

--- lib/intent/fetch_data.dart
+++ lib/intent/fetch_data.dart
@@ -12,7 +12,7 @@ class FetchAppData {
     DocumentSnapshot available = await getTodayAvailable();
     DocumentSnapshot reservationForDate = await getTodayReservationForDate();
 
-    // print(reservationForDate.data());
+    print(reservationForDate.data());
 
     Map<String, dynamic> data = available.data() as Map<String, dynamic>;
     List<dynamic> notParsed = data['time'];
@@ -22,22 +22,22 @@ class FetchAppData {
     Map<String, dynamic> firebaseReservations =
         reservationForDate.data() as Map<String, dynamic>;
     appState.reservations.reservations[SectionName.ga]!.reserved.addAll(
-        (firebaseReservations["SectionName.ga"] as List<dynamic>)
+        (firebaseReservations['SectionName']['ga'] as List<dynamic>)
             .map((e) => int.parse(e.toString())));
     appState.reservations.reservations[SectionName.na]!.reserved.addAll(
-        (firebaseReservations["SectionName.na"] as List<dynamic>)
+        (firebaseReservations["SectionName"]['na'] as List<dynamic>)
             .map((e) => int.parse(e.toString())));
     appState.reservations.reservations[SectionName.da]!.reserved.addAll(
-        (firebaseReservations["SectionName.da"] as List<dynamic>)
+        (firebaseReservations["SectionName"]['da'] as List<dynamic>)
             .map((e) => int.parse(e.toString())));
     appState.reservations.reservations[SectionName.ra]!.reserved.addAll(
-        (firebaseReservations["SectionName.ra"] as List<dynamic>)
+        (firebaseReservations["SectionName"]['ra'] as List<dynamic>)
             .map((e) => int.parse(e.toString())));
     appState.reservations.reservations[SectionName.ma]!.reserved.addAll(
-        (firebaseReservations["SectionName.ma"] as List<dynamic>)
+        (firebaseReservations["SectionName"]['ma'] as List<dynamic>)
             .map((e) => int.parse(e.toString())));
 
-    // print(appState.reservations.reservations[SectionName.na]!.reserved);
+    print(appState.reservations.reservations[SectionName.na]!.reserved);
   }
 
   static Future<void> updateUserData(String id) async {
